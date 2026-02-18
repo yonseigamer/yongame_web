aa2199aa8bc8e6286cb56aa7fe6461e7edaf7750
yonseigamer <yonseigamer@gmail.com>
2026-02-19 01:35:04 +0900

    fix: revert to github pages default URL

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/yongame_web/',
   server: {
     port: 3000,
     open: true,
